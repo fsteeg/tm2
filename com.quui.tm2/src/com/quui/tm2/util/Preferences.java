9447e6d7348d753ce4b6a39a04b31d27e88dd2c1
Fabian Steeg <fsteeg@gmail.com>
2011-04-30 02:10:52 +0200

    Simplified and documented setup

--- com.quui.tm2/src/com/quui/tm2/util/Preferences.java
+++ com.quui.tm2/src/com/quui/tm2/util/Preferences.java
@@ -12,6 +12,7 @@ public class Preferences {
     }
 
     public enum Default implements Key {
+    	PROJECT("project"),
         GOLD("default_gold"),
         NAME("default_name"),
         CORPUS("default_corpus"),
@@ -44,10 +45,10 @@ public class Preferences {
         InputStream stream;
         try {
             stream = Thread.currentThread().getContextClassLoader()
-                    .getResourceAsStream("amas.properties");
+                    .getResourceAsStream("tm2.properties");
             if (stream == null) {
                 throw new IllegalStateException(
-                        "Please copy the amas.properties.template file to amas.properties and set your local settings");
+                        "Please copy the tm2.properties.template file to tm2.properties and set your local settings");
             }
             properties = new Properties();
             properties.load(stream);
@@ -59,8 +60,16 @@ public class Preferences {
         }
     }
 
-    public static String get(Key key) {
-        String property = properties.getProperty(key.getKey());
-        return property;
-    }
+	public static String get(Key key) {
+		String property = "";
+		if (key.equals(Default.PROJECT) || key.equals(Environment.DOT_HOME)
+				|| key.equals(Environment.DOT_FORMAT)
+				|| key.equals(Default.NAME)) {
+			property = properties.getProperty(key.getKey());
+		} else {
+			property = properties.getProperty(Default.PROJECT.getKey()) + "/"
+					+ properties.getProperty(key.getKey());
+		}
+		return property;
+	}
 }
